e7d9095e9bbd42718d223b1ce49540eb20fb62ac
Andrew McCloud <mcclouda@wncc.edu>
2023-09-01 22:56:13 -0600

    Add comments

--- Module 5/AndrewCustomers.php
+++ Module 5/AndrewCustomers.php
@@ -1,3 +1,15 @@
+<!--
+
+Andrew McCloud
+
+September 1, 2023
+
+Professor Voelcker
+
+Module 5 Coding Assignment
+
+-->
+
 <?php 
 
 $customers = array(
@@ -66,36 +78,62 @@ $customers = array(
                         <!-- The PHP code for when the submit button is clicked -->
                         <?php
                         
+                            // If the submit button is clicked
                             if(isset($_POST['submit'])){
 
+                                // Create a counter to display an error if there are no results 
                                 $counter = 0;
 
+                                // Create a variable named $choice and
+                                // assign it to the value chosen in the dropdown menu
                                 $choice = $_POST['criteria'];
 
+                                // Create a variable named $search and
+                                // assign it to the value that was typed into the criteriaTxt textbox
                                 $search = $_POST['criteriaTxt'];
 
-                                $newWord = "";
+                                $regex = '^[A-Z0-9]{3}-[A-Z0-9]{3}-[A-Z0-9]{4}^';
 
+                                // Use an if statement to see if the user clicked on the phone-number option
                                 if($choice == 'phone-number'){
 
-                                    if(strlen($search) == 10){
+                                    // If the phone number that was typed in
+                                    // does not match the format of xxx-xxx-xxxx
+                                    if(!(preg_match($regex, $search))){
 
-                                        $search = preg_replace("/^(\d{3})(\d{3})(\d{4})$/", "$1-$2-$3", $search);
+                                        // Use one more if statement to check if the phone number
+                                        // is equal to 10 (the length of a standard phone number)
+                                        if(strlen($search) == 10){
+
+                                            // Format the string that was typed in to xxx-xxx-xxxx
+                                            $search = preg_replace("/^(\d{3})(\d{3})(\d{4})$/", "$1-$2-$3", $search);
+    
+                                        }
 
                                     }
 
                                 }
 
+                                // Use foreach to loop through the customer array
+                                // that is filled with single customer arrays
                                 foreach($customers as $customer){
 
+                                    // Use array search to search through each individual array 
+                                    // in the customers array and check if it is equal to the text
+                                    // typed into the textbox
                                     if((array_search($search, $customer)) == $choice){
 
+                                        // Add 1 to the counter variable
                                         $counter++;
 
+                                        // Close the php tag so we can use html in this foreach loop
                                         ?>
 
+                                        <!-- Create a new row in the display search table -->
                                         <tr class="text-center">
 
+                                            <!-- Use td and echo the 4 values from the customer array_search
+                                                 that contains the search value -->
                                             <td><?php echo $customer['first-name'] ?></td>
                                             <td><?php echo $customer['last-name'] ?></td>
                                             <td><?php echo $customer['age'] ?></td>
@@ -104,16 +142,20 @@ $customers = array(
 
                                         </tr>
 
+                                        <!-- Create a new php tag -->
                                         <?php
 
                                     }
 
                                 }
 
+                                // Use an if statement to check if the counter variable equals 0
                                 if($counter == 0){
 
                                     ?>
 
+                                        <!-- Create a new row that displays 1 cell that spans over
+                                            all 4 tables columns and display an error for no search results -->
                                         <tr><td colspan="4" id="errorCol"><b>No Results Found.</b></td></tr>
 
                                     <?php
@@ -128,16 +170,20 @@ $customers = array(
 
                 </div>
 
+                <!-- Create a form for the inputs from the user -->
                 <form class="form text-center" action="<?php $_SERVER['PHP_SELF']; ?>" method="post">
-
+                    <!-- Create a div to hold the dropdown -->
                     <div class="form-outline mb-4">
 
+                        <!-- Create a label for the dropdown -->
                         <label class="form-label mt-3" for="headerNames"><b>Choose Criteria:</b></label>
 
                         <br>
 
+                        <!-- Use select with enclosed options to create a dropdown -->
                         <select name="criteria" id="criteria" required>
 
+                            <!-- This option will display when the page is loaded. It can not be chosen -->
                             <option value="" selected disabled>-- Choose Criteria --</option>
 
                             <option value="first-name" name="first-name">First Name</option>
@@ -152,22 +198,27 @@ $customers = array(
 
                     </div>
 
+                    <!-- Create a div to hold the text input box from the user -->
                     <div class="form-outline mb-4">
 
+                        <!-- The text box for the user input to search a specific text -->
                         <input type="text" name="criteriaTxt" placeholder="Criteria.." required>
 
                         <br>
 
                     </div>
 
+                    <!-- Create a submit button -->
                     <button class="btn btn-primary my-2" type="submit" name="submit">Submit</button>
 
                 </form>
 
             </div>
 
+            <!-- Create a div to hold the example table so users can test the search functionality -->
             <div class="mt-5" id="customerTable">
 
+                <!-- Create a div specifically for the custom text at the top -->
                 <div class="text-center mt-2 mx-2" id="cTableTitle">
 
                     <h2>List of all the customers</h2>
@@ -175,10 +226,12 @@ $customers = array(
 
                 </div>
 
+                <!-- Create a div for the table -->
                 <div class="mx-2">
 
                     <table class="table table-bordered table-striped text-center">
 
+                        <!-- Give the table headers -->
                         <tr id="tableHeader">
 
                             <th>First Name</th>
@@ -188,12 +241,14 @@ $customers = array(
 
                         </tr>
 
+                            <!-- Our PHP code to display the array values -->
                             <?php
                             
+                                // Use foreach to loop through the $customer array full of arrays
                                 foreach($customers as $customer){
 
                                     ?>
-
+                                        <!-- Display each array and their values -->
                                         <tr>
 
                                             <td><?php echo $customer['first-name']; ?></td>
@@ -217,6 +272,7 @@ $customers = array(
 
         </div>
 
+        <!-- Give our webpage some pizzazz -->
         <style>
 
             #errorCol{
